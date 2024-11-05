c0c6f837c8f7d9f2aa489e619b36aadf8cacfad2
P31Dead <P31Dead@LAPETOPE>
2024-11-06 00:50:19 +0300

    added list job init

--- DBProject/EmplAdd.xaml.cs
+++ DBProject/EmplAdd.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +21,49 @@ namespace DBProject
     /// </summary>
     public partial class EmplAdd : Window
     {
-        public EmplAdd()
+        string connectionStr;
+        public EmplAdd(string connectionStr)
         {
+            this.connectionStr = connectionStr;
             InitializeComponent();
+            InitJobTitles();
+        }
+
+        private void InitJobTitles()
+        {
+            string query = "SELECT jobID, jobName FROM JobTitles";
+            using(SqlConnection connection=new SqlConnection(connectionStr))
+            {
+                try
+                {
+                    connection.Open();
+                    SqlDataAdapter adapter = new SqlDataAdapter(query, connectionStr);
+                    DataSet ds = new DataSet();
+                    adapter.Fill(ds);
+                    jobList.ItemsSource = ds.Tables[0].Rows;
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+        private void AddNewEmployee(object sender, RoutedEventArgs e)
+        {
+
+            string sqlQueryAddEmpl = $"EXEC AddNewEmploye ";
+            using (SqlConnection connection=new SqlConnection(connectionStr))
+            {
+                try
+                {
+                    SqlCommand addEmpl = new SqlCommand(sqlQueryAddEmpl, connection);
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                
+            }
         }
     }
 }
